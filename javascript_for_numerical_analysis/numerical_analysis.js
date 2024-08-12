2f051323c6f0d0fc9c2cae890cb2b19aa2a8f22f
pi <95388191+pi826@users.noreply.github.com>
2024-08-13 02:08:56 +0900

    kadaidayo

---
+++ javascript_for_numerical_analysis/numerical_analysis.js
@@ -0,0 +1,21 @@
+
+
+console.log("Hello JavaScript!!");
+
+console.log("Hello")
+console.log("World!")
+
+let x;
+x = "message";
+console.log(x);
+
+
+let a = "b";
+
+let cc = a;
+
+cc = "a";
+const math = "good";
+
+console.log(a);
+console.log(cc);
